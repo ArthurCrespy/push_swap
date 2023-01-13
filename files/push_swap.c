b798d44401d9329bc69ce88deb66dc25f45bbed1
CRESPY Arthur <arthur@crespy.eu>
2023-01-13 18:21:08 +0100

    Add main and push_swap.c/.h files

---
+++ files/push_swap.c
@@ -0,0 +1,5 @@
+//
+// Created by Arthur on 13/01/2023.
+//
+
+#include "push_swap.h"
---
+++ files/push_swap.h
@@ -0,0 +1,10 @@
+//
+// Created by Arthur on 13/01/2023.
+//
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+
+
+#endif

df748bdc5afa7e7c71fa5f92179f33bb53f2626f
CRESPY Arthur <arthur@crespy.eu>
2023-01-16 16:17:59 +0100

    Add useful functions

--- files/utils.c
+++ files/utils.c
@@ -12,6 +12,21 @@
 
 #include "push_swap.h"
 
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] == s2[i])
+			i++;
+		else
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
 int	ft_strlen(const char *str)
 {
 	int	i;
@@ -20,4 +35,39 @@ int	ft_strlen(const char *str)
 	while (str[i])
 		i++;
 	return (i);
+}
+
+
+static int	ft_isspace(char c)
+{
+	if ((c >= 9 && c <= 13) || c == 32)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	if (!str)
+		return (0);
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] != 0x00 && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result *= 10;
+		result += (str[i++] - '0');
+	}
+	return (result * sign);
 }

d2bc8cae43d0cf87ff82b03744e9b416ebda6851
CRESPY Arthur <arthur@crespy.eu>
2023-01-16 21:18:12 +0100

    Call sort_stack()

--- files/push_swap.c
+++ files/push_swap.c
@@ -6,16 +6,16 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 14:18:04 by acrespy           #+#    #+#             */
-/*   Updated: 2023/01/16 14:18:23 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/01/16 16:21:29 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int    push_swap(int i_stack_a, char **stack_a)
+int	push_swap(int i_stack_a, char **stack_a)
 {
 	if (check_args(i_stack_a, stack_a))
 		return (1);
-
+	sort_stack(i_stack_a, stack_a);
 	return (0);
 }

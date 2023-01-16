126628425f178251d39fae714a77661ae4eed3be
CRESPY Arthur <arthur@crespy.eu>
2023-01-16 15:07:34 +0100

    Add prototypes

--- files/push_swap.h
+++ files/push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/16 14:18:02 by acrespy           #+#    #+#             */
+/*   Updated: 2023/01/16 14:18:23 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //
 // Created by Arthur on 13/01/2023.
 //
@@ -5,6 +17,10 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include <stdio.h>
 
+void    push_swap(int argc, char **argv);
+
+int	ft_strlen(const char *str);
 
 #endif

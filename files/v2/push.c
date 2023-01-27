3ba230c1896aff281e893f479ece5da3b01c4b3e
CRESPY Arthur <arthur@crespy.eu>
2023-01-27 10:01:40 +0100

    Add push() functions

---
+++ files/v2/push.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 19:08:31 by acrespy           #+#    #+#             */
+/*   Updated: 2023/01/24 19:10:20 by acrespy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	pa(t_node **stack_a, t_node **stack_b)
+{
+	t_node	*tmp;
+
+	if (*stack_b == NULL)
+		return ;
+	tmp = *stack_b;
+	*stack_b = (*stack_b)->next;
+	tmp->next = *stack_a;
+	*stack_a = tmp;
+	print_func("pa");
+}
+
+void	pb(t_node **stack_a, t_node **stack_b)
+{
+	t_node	*tmp;
+
+	if (*stack_a == NULL)
+		return ;
+	tmp = *stack_a;
+	*stack_a = (*stack_a)->next;
+	tmp->next = *stack_b;
+	*stack_a = tmp;
+	print_func("pb");
+}

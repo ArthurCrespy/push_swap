cb7318ac710dc0af03fc1e13f13c41f87cb7987e
CRESPY Arthur <arthur@crespy.eu>
2023-01-27 10:02:29 +0100

    Norminette issues

--- files/v2/sanitize.c
+++ files/v2/sanitize.c
@@ -6,7 +6,7 @@
 /*   By: acrespy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 15:14:22 by acrespy           #+#    #+#             */
-/*   Updated: 2023/01/17 15:14:23 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/01/24 19:02:35 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ void	sanitize_content_stack(char **stack_a)
 	}
 }
 
-void    sanitize_i_stack(int i_stack_a)
+int		sanitize_i_stack(int i_stack_a)
 {
-	i_stack_a--;
+	return (i_stack_a - 2);
 }
 
-void    sanitize_stack(int i_stack_a, char **stack_a)
+int		sanitize_stack(int i_stack_a, char **stack_a)
 {
 	sanitize_content_stack(stack_a);
-	sanitize_i_stack(i_stack_a);
+	return (sanitize_i_stack(i_stack_a));
 }

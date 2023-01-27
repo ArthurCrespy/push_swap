2857f6c65e7b69628ad5f527fa790814203f7b0a
CRESPY Arthur <arthur@crespy.eu>
2023-01-27 12:08:36 +0100

    push_swap test function

--- files/v2/push_swap.c
+++ files/v2/push_swap.c
@@ -6,44 +6,16 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 14:18:04 by acrespy           #+#    #+#             */
-/*   Updated: 2023/01/24 19:08:46 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/01/27 11:35:27 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
 
-void	little_stack_sort(t_node **stack_a)
-{
-	int a;
-	int b;
-	int c;
-
-	a = (*stack_a)->data;
-	b = (*stack_a)->next->data;
-	c = (*stack_a)->next->next->data;
-
-	if (a > b && b > c)
-	{
-		sa(*stack_a);
-		rra(stack_a);
-	}
-	if (a > b && b < c && a > c)
-		double_rra(*stack_a);
-	if (a < b && b > c)
-		rra(stack_a);
-	if (a < b && b < c)
-	{
-		rra(stack_a);
-		sa(*stack_a);
-	}
-	if (a > b && b < c)
-		sa(*stack_a);
-}
 
 int	push_swap(int i_stack_a, char **c_stack_a)
 {
-	t_node *stack_a;
-	t_node *stack_b;
+	t_node	*stack_a;
+	t_node	*stack_b;
 
 	stack_a = NULL;
 	stack_b = NULL;
@@ -60,9 +32,9 @@ int	push_swap(int i_stack_a, char **c_stack_a)
 	print_list(stack_a);
 	printf("\n");
 
-	ra(&stack_a);
+	//ra(&stack_a);
 
-	//little_stack_sort(&stack_a);
+	little_stack_sort(&stack_a);
 
 	printf("stack_a after: ");
 	print_list(stack_a);

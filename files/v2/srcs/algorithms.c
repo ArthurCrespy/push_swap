75725e355adae59e2dba69cae4a5f02db3234c39
CRESPY Arthur <arthur@crespy.eu>
2023-02-05 21:51:53 +0100

    Add medium_stack_sort algo

--- files/v2/srcs/algorithms.c
+++ files/v2/srcs/algorithms.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../includes/push_swap.h"
 
 void	little_stack_sort(t_node **stack_a)
 {
@@ -39,5 +39,74 @@ void	little_stack_sort(t_node **stack_a)
 	else if (a > b && b < c)
 		sa(*stack_a);
 }
+
+int find_median(t_node *stack_a, t_node *stack_b, int len)
+{
+	t_node *temp;
+	int median;
+	int i;
+	int j;
+
+	temp = stack_a;
+	median = temp->data;
+	i = 0;
+	while (i < len / 2)
+	{
+		j = 0;
+		temp = stack_a;
+		while (j < len)
+		{
+			if (temp->data < median)
+			{
+				pb(&stack_a, &stack_b);
+				len--;
+			}
+			else
+			{
+				ra(&stack_a);
+				j++;
+			}
+		}
+		median = stack_b->data;
+		pb(&stack_b, &stack_a);
+		len = len / 2;
+		i++;
+	}
+	return (median);
+}
+
+
+void medium_stack_sort(t_node **stack_a, t_node **stack_b, int len)
+{
+	int median;
+	int i;
+	int count;
+
+	if (len <= 1)
+		return ;
+	median = find_median(*stack_a, *stack_b ,len);
+	count = 0;
+	i = 0;
+	while (count < len)
+	{
+		if ((*stack_a)->data < median)
+		{
+			pb(stack_a, stack_b);
+			len--;
+		}
+		else
+		{
+			ra(stack_a);
+			i++;
+			count++;
+		}
+	}
+	medium_stack_sort(stack_b, stack_a, len / 2);
+	medium_stack_sort(stack_a, stack_b, i);
+	while (i-- > len / 2)
+		rr(stack_a, stack_b);
+}
+
+
 
 

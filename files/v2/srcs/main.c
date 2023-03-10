bc9a86c9a50ae0c8bbeebcc7b6ee9bf36c0ee84b
CRESPY Arthur <arthur@crespy.eu>
2023-03-10 16:38:48 +0100

    Update main.c

--- files/v2/srcs/main.c
+++ files/v2/srcs/main.c
@@ -6,15 +6,15 @@
 /*   By: acrespy <acrespy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 14:17:56 by acrespy           #+#    #+#             */
-/*   Updated: 2023/01/16 20:11:34 by acrespy          ###   ########.fr       */
+/*   Updated: 2023/03/10 16:01:56 by acrespy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void    free_stacks(t_data **stack_a, t_data **stack_b)
+void	free_stacks(t_data **stack_a, t_data **stack_b)
 {
-	t_data *tmp;
+	t_data	*tmp;
 
 	while (*stack_a)
 	{
@@ -30,21 +30,20 @@ void    free_stacks(t_data **stack_a, t_data **stack_b)
 	}
 }
 
-int		main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_data *stack_a = NULL;
-	t_data *stack_b = NULL;
+	t_data	*stack_a;
+	t_data	*stack_b;
 
+	stack_a = NULL;
+	stack_b = NULL;
 	if (check_args(argc, argv))
 		return (1);
 	create_stack(&stack_a, argc, argv);
 	if (ft_nodelen(stack_a) == 3)
 		little_stack_sort(&stack_a);
-	else if (ft_nodelen(stack_a) == 5)
-		medium_stack_sort(&stack_a, &stack_b);
 	else
-		large_stack_sort(&stack_a, &stack_b, ft_nodelen(stack_a));
+		large_stack_sort(&stack_a, &stack_b, ft_nodelen(stack_a) / 2);
 	write(1, "\n", 1);
-	//free_stacks(&stack_a, &stack_b);
 	return (0);
 }
